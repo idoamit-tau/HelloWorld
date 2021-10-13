c4f0fb69fc7ce7e27457e6e3d228346ae5e75366
idoam <idoamit1@mail.tau.ac.il>
2021-10-13 18:20:55 +0300

    "first commit"

--- src/LenaHelp.java
+++ src/LenaHelp.java
@@ -7,16 +7,22 @@ public class LenaHelp {
         s = (short)i;
 
         int j = i << 16;
-
-        System.out.println("1 - " + j);
+        System.out.println("i represents in bytes - " + Integer.toBinaryString(i));
+        String ido =  Integer.toBinaryString(i);
+        System.out.println(ido.length());
 
         System.out.println("1 - " + Integer.toBinaryString(j));
 
+        System.out.println("1 - " + j);
+
+
         j = j >> 16;
+
+        System.out.println("2 - " + Integer.toBinaryString(j));
 
         System.out.println("2 - " + j);
 
-        System.out.println("2 - " + Integer.toBinaryString(j));
+
 
         System.out.println("---------------");
 
